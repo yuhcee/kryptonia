5ce005d274b18fafc623c9b09f5531c619ddc656
yuhcee <egbouchenna001@gmail.com>
2022-05-15 21:33:46 +0100

    adjust responsiveness

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -25,10 +25,10 @@ const Welcome = () => {
     const handleSubmit = () => {};
 
     return (
-        <div className="flex w-full justify-center items-center">
-            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex p-2 mf:flex-row flex-col w-full justify-center items-center">
+            <div className="flex flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
-                    <h1 className="text-3xl sm:text-6xl text-white text-gradient py-1">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br />
                         across the world
                     </h1>
@@ -49,8 +49,8 @@ const Welcome = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-                <div className="flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full p-3 my-5 eth-card white-glassmorphism">
+            <div className="flex flex-col flex-1  items-center justify-center p-10 w-full mf:mt-0 mt-10">
+                <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                     <div className="flex justify-between flex-col w-full h-full">
                         <div className="flex justify-between items-start">
                             <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
