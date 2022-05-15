33e39c872e0f24c1a277069c404b7cda2f47448b
yuhcee <egbouchenna001@gmail.com>
2022-05-15 21:34:31 +0100

    update tailwindCss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,35 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    mode: 'jit',
+    // darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            display: ['Barlow', 'sans-serif'],
+            body: ['Barlow', 'sans-serif'],
+        },
+        extend: {
+            screens: {
+                mf: '990px',
+            },
+            keyframes: {
+                'slide-in': {
+                    '0%': {
+                        '-webkit-transform': 'translateX(120%)',
+                        transform: 'translateX(120%)',
+                    },
+                    '100%': {
+                        '-webkit-transform': 'translateX(0%)',
+                        transform: 'translateX(0%)',
+                    },
+                },
+            },
+            animation: {
+                'slide-in': 'slide-in 0.5s ease-out',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+};
