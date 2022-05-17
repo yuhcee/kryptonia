abd229e9f9d1a95ba005bad787f50c8f379626cc
yuhcee <egbouchenna001@gmail.com>
2022-05-17 19:25:48 +0100

    update: display address

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
+import { TransactionContext } from '../context/TransactionContext';
+
 import { Loader } from '.';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const companyCommonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
@@ -19,7 +24,7 @@ const Input = ({ placeholder, name, handleChange, type, value }) => (
 );
 
 const Welcome = () => {
-    const connectWallet = () => {};
+    const { connectWallet, connectedAccount } = useContext(TransactionContext);
 
     const handleChange = () => {};
     const handleSubmit = () => {};
@@ -33,10 +38,12 @@ const Welcome = () => {
                         across the world
                     </h1>
                     <p className="text-left mt-5 text-xl text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell cryptocurrencies easily online on Krypto.</p>
-                    <button className="flex flex-row justify-center items-center my-5 bg-[#2952e3] w-full p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" type="button" onClick={connectWallet}>
-                        <AiFillPlayCircle className="text-white mr-2" />
-                        <p className="font-semibold text-white text-base">Connect Wallet</p>
-                    </button>
+                    {!connectedAccount && (
+                        <button className="flex flex-row justify-center items-center my-5 bg-[#2952e3] w-full p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" type="button" onClick={connectWallet}>
+                            <AiFillPlayCircle className="text-white mr-2" />
+                            <p className="font-semibold text-white text-base">Connect Wallet</p>
+                        </button>
+                    )}
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${companyCommonStyles}`}> Reliability</div>
@@ -50,16 +57,16 @@ const Welcome = () => {
             </div>
 
             <div className="flex flex-col flex-1 items-center justify-center p-10 w-full mf:mt-0 mt-10">
-                <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                <div className="p-3 flex justify-end items-start flex-col font-semibold rounded-xl h-48 sm:w-80 w-full my-5 eth-card white-glassmorphism">
                     <div className="flex justify-between flex-col w-full h-full">
                         <div className="flex justify-between items-start">
                             <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                                <SiEthereum fontSize={21} color={'#fff'} />
+                                <SiEthereum fontSize={24} color={'#fff'} />
                             </div>
-                            <BsInfoCircle fontSize={17} color="#fff" />
+                            <BsInfoCircle fontSize={20} color="#fff" />
                         </div>
                         <div>
-                            <p className="font-light text-sm text-white">Current Account</p>
+                            <p className="font-light text-lg text-white">{shortenAddress(connectedAccount)}</p>
                             <p className="font-semi-bold text-lg mt-1 text-white">Ethereum</p>
                         </div>
                     </div>
