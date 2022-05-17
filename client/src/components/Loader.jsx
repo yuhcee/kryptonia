52f642747c26e5ba6c3608d0ce37ff405e09b514
yuhcee <egbouchenna001@gmail.com>
2022-05-17 16:20:31 +0100

    fix: Loader w/h

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => (
-    <div className="flex justify-center items-center py-3">
-        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-red-700" />
+    <div className="flex justify-center items-center ">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-red-700" />
     </div>
 );
 export default Loader;
