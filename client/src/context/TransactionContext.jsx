89b44753eb05307cd8370b196003169b2e80a83a
yuhcee <egbouchenna001@gmail.com>
2022-05-17 19:24:02 +0100

    feat: add connectWallet

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -15,5 +15,37 @@ const getEthereumContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
-    
+    const [connectedAccount, setConnectedAccount] = useState('');
+
+    useEffect(() => {
+        const checkIfWalletIsConnected = async () => {
+            try {
+                if (!ethereum) return alert('Please install MetaMask');
+                const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+                if (accounts.length) {
+                    setConnectedAccount(accounts[0]);
+                    // getAllTransactions()
+                } else {
+                    console.log('No accounts found');
+                }
+            } catch (error) {
+                console.log(error);
+                throw new Error('No ethereum object.');
+            }
+        };
+        checkIfWalletIsConnected();
+    }, []);
+
+    const connectWallet = async () => {
+        try {
+            if (!ethereum) return alert('Please install MetaMask');
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            setConnectedAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+            throw new Error('No ethereum object.');
+        }
+    };
+    return <TransactionContext.Provider value={{ connectWallet, connectedAccount }}>{children}</TransactionContext.Provider>;
 };
