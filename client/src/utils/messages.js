645cac1fa4cf185b50635d5202562c82b7ac6d2b
yuhcee <egbouchenna001@gmail.com>
2022-05-28 01:47:51 +0100

    utils: add loader messages

---
+++ client/src/utils/messages.js
@@ -0,0 +1,12 @@
+export const messages = [
+    'Please wait...',
+    'Your transaction is processing...',
+    'Transaction in progress...',
+    'Almost done...',
+    'Almost there...!',
+    'Kindly, hold...',
+    'This may take a while...',
+    'Any minute now...',
+    'Transaction in queue...',
+    'Transaction processing...',
+];
