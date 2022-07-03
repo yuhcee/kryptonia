5cc9e4456a2bd5d08b1e82c76a91e7d2174bb06f
yuhcee <egbouchenna001@gmail.com>
2022-07-03 20:39:50 +0100

    refactor(useFetch):fix default url error

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from 'react';
 
 const APIKEY = import.meta.env.VITE_GIPHY_API;
 
-const useFetch = ({ keyword }) => {
+const useFetch = ({ keyword, url }) => {
     const [gifUrl, setGifUrl] = useState('');
 
     useEffect(() => {
         const fetchGifs = async () => {
             try {
-                const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(' ').join('')}&limit=1`);
-                const { data } = await response.json();
+                if (keyword) {
+                    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(' ').join('')}&limit=1`);
+                    const { data } = await response.json();
 
-                setGifUrl(data[0]?.images?.downsized_medium.url);
+                    setGifUrl(data[0]?.images?.downsized_medium?.url);
+                } else {
+                    if (!url) {
+                        setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284');
+                    }
+                    setGifUrl(url);
+                }
             } catch (error) {
-                setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284');
+                console.error(error);
             }
         };
         fetchGifs();
